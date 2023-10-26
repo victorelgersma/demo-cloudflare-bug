95027e11bbb2367b9a27f3972e3728b53d99be6e
Shaun Hall <shaun.hall@thehutgroup.com>
2023-10-26 09:20:43 +0100

    deno

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import cloudflare from "@astrojs/cloudflare";
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   output: "server",
-  adapter: cloudflare()
+  adapter: deno()
 });
